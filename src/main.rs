cabb61c0476ad641424aaa30238acbea6a8e1536
KoBruhh <atomoyuncu.yt@gmail.com>
2023-03-25 10:57:16 +0300

    rascii does not exist. debugged

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use rascii::{
-    convert,
-};
+mod lib;
+use lib::convert;
 
 fn main() {
     convert(); // second parameter takes a boolean to make the image colored, third one takes a boolean to reverse the image (prevent black background).
