07136c3a347bed851075d00ccefa09d6b906c059
RGBCube <RGBCube@users.noreply.github.com>
2023-05-26 20:12:48 +0300

    "Nuke rust code"ü

--- src/main.rs
+++
@@ -1,6 +0,0 @@
-mod lib;
-use lib::convert;
-
-fn main() {
-    convert(); // second parameter takes a boolean to make the image colored, third one takes a boolean to reverse the image (prevent black background).
-}
