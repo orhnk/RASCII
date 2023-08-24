b3441cbc2bd7fa63164893b67320fec43d7b0b49
utfeight <utfeightt@gmail.com>
2023-08-24 18:14:59 +0300

    fix(#18): remove redundant absolute_path
    The crate image can already works with absolute paths so remove the
    implementation

--- src/lib.rs
+++ src/lib.rs
@@ -26,23 +26,18 @@ mod gif_renderer;
 mod image_renderer;
 mod renderer;
 
-use std::{
-    io,
-    path::Path,
-};
-use expanduser::expanduser;
 use image::DynamicImage;
 use image_renderer::ImageRenderer;
 pub use renderer::RenderOptions;
 use renderer::Renderer;
+use std::{io, path::Path};
 
 pub fn render<P: AsRef<Path> + AsRef<str>>(
     path: P,
     to: &mut impl io::Write,
     options: &RenderOptions<'_>,
 ) -> image::ImageResult<()> {
-    let absolute_path = expanduser(&path)?;
-    let image = &image::open(absolute_path)?;
+    let image = &image::open(path)?;
     render_image(image, to, &options)
 }
 
@@ -61,8 +56,7 @@ pub fn render_to<P: AsRef<Path> + AsRef<str>>(
     buffer: &mut String,
     options: &RenderOptions<'_>,
 ) -> image::ImageResult<()> {
-    let absolute_path = expanduser(&path)?;
-    let image = &image::open(absolute_path)?;
+    let image = &image::open(path)?;
     let renderer = ImageRenderer::new(image, options);
     renderer.render_to(buffer)?;
     Ok(())
