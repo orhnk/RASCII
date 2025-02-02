b5b81d24d0ad19c836e4918c099ad4f5ee827ab1
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-02-03 00:03:17 +0530

    Added  and  to

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 echo "Sudo is needed to copy the binary to /usr/local/bin - prompting now."
 sudo true
 
+git clone https://github.com/orhnk/RASCII
+cd RASCII
+
 cargo build --release
 
 sudo cp ./target/release/rascii /usr/local/bin
